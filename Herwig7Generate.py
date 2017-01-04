cef4d5382d1f1bed4bd044b30fbc9e940c369128
Ludovic Michel Scyboz <lscyboz@draco01.mpcdf.mpg.de>
2017-01-04 13:53:21 +0100

    With submit job options at the beginning

--- Herwig7Generate.py
+++ Herwig7Generate.py
@@ -5,12 +5,12 @@ import os
 from time import gmtime, strftime
 import glob
 
-nEvPerFile = 10000
-nRuns = 1000
+nEvPerFile = 2000
+nRuns = 500
 
-dir = "/ptmp/lscyboz/Herwig_WWbB_"+strftime("H:%M:%S", gmtime())+"/"
+dir = "/ptmp/lscyboz/Herwig_WWbB_"+strftime("%H-%M-%S", gmtime())+"/"
 InputFolder = "/ptmp/lscyboz/HERWIG/"
-WorkFolder = "/ptmp/lscyboz"
+WorkFolder = "/ptmp/lscyboz/"
 
 SettingsFolder    = "/ptmp/lscyboz/Settings/"
 SetupFileNameGen    = "setupfile.in"
@@ -20,16 +20,27 @@ pars 		= "/afs/ipp-garching.mpg.de/home/l/lscyboz/mc/"
 def SubDirPath (d):
     return filter(os.path.isdir, [os.path.join(d,f) for f in os.listdir(d)])
 
-def printSetupLinesInSubmitFileRivet(file):
+def printSetupLinesInSubmitFileRivet(file, str):
 
 #    setupLines = ["export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase/ \n",
 #                  "source /cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase/user/atlasLocalSetup.sh \n",
 #                  "asetup 20.8.2 \n"
 #                  ]
 
-    setupLines = ["source /ptmp/lscyboz/Herwig-7.0.3/bin/activate\n",
-		  "source /afs/ipp-garching.mpg.de/home/l/lscyboz/Herwig-7.0.3/src/Rivet-2.4.0/rivetenv.sh\n",
-		  "export RIVET_ANALYSIS_PATH=/afs/ipp-garching.mpg.de/home/l/lscyboz/RivetCustomAnalyses/:$RIVET_ANALYSIS_PATH\n"]
+    setupLines = ["#!/bin/bash -l\n",
+		  "#SBATCH -o ./tjob.out.%j\n",
+		  "#SBATCH -e ./tjob.err.%j\n",
+		  "#SBATCH -D ./\n",
+		  "#SBATCH -J Herwig_"+str+"\n",
+		  "#SBATCH --partition=general\n",
+		  "#SBATCH --nodes=8\n",
+		  "#SBATCH --ntasks-per-node=32\n",
+		  "#SBATCH --mail-type=none\n",
+		  "#SBATCH --mail-user=<userid>@rzg.mpg.de\n",
+		  "#SBATCH --time=02:00:00\n",
+		  "source /ptmp/lscyboz/HERWIG/bin/activate\n",
+		  "source /ptmp/lscyboz/RIVET-2.5.1/rivetenv.sh\n",
+		  "export RIVET_ANALYSIS_PATH=/ptmp/lscyboz/RivetCustomAnalyses/:$RIVET_ANALYSIS_PATH\n"]
 
     file.writelines(setupLines)
 
@@ -54,7 +65,7 @@ def SubmitHerwigJob(nEvents, seed, InputFileNameGen):
 
     submitFileNameSH = WorkFolder+"Event_"+specStr+".sh"
 
-    redo = false
+    redo = False
     if os.path.exists(OutputYoda):
       f=open(OutputYoda,'r')
       for i, line in enumerate(f):
@@ -71,10 +82,10 @@ def SubmitHerwigJob(nEvents, seed, InputFileNameGen):
     if redo==True:
 
         submitfile2 = open(submitFileNameSH, "w")
-        printSetupLinesInSubmitFileRivet(submitfile2)
+        printSetupLinesInSubmitFileRivet(submitfile2, specStr)
 
         codeLines2 = []
-#       codeLines2.append("mkdir -p "+tmpFolder)
+
         codeLines2.append("cd "+InputFolder)
         codeLines2.append("mkdir -p "+tmp)
         codeLines2.append("echo 'set /Herwig/Generators/EventGenerator:RandomNumberGenerator:Seed "+str(seed)+"' >> "+OutputFolder+SetupFileNameGen)
@@ -82,8 +93,9 @@ def SubmitHerwigJob(nEvents, seed, InputFileNameGen):
 
         codeLines2.append("Herwig run "+InputFileNameGen+" -N "+str(nEvents)+" -x "+OutputFolder+SetupFileNameGen)
 
-        analyses=""
-                analyses += " -a "+routines
+        analyses=" -a bB4l_comparison -a MC_DENNER -a ATLAS_sherpa"
+
+	codeLines2.append("rivet"+analyses+" "+tmp+OutputFile+" -H "+OutputYoda)
 
         for codeLine in codeLines2:
             submitfile2.write(codeLine+" \n")
@@ -94,7 +106,7 @@ def SubmitHerwigJob(nEvents, seed, InputFileNameGen):
 
         cmd = "chmod a+x " + submitFileNameSH
         os.system(cmd)
-        cmd = "sbatch "+ submitFileNameSH
+#        cmd = "sbatch "+ submitFileNameSH
         os.system(cmd)
 
         return True
@@ -105,7 +117,7 @@ def SubmitHerwigJob(nEvents, seed, InputFileNameGen):
 
 ## Options file for systematic generation: the user should set the settings required for the different runs there
 
-os.system("source /ptmp/lscyboz/Rivet-2.5.0/rivetenv.sh")
+os.system("source /ptmp/lscyboz/RIVET-2.5.1/rivetenv.sh")
 os.system("export RIVET_ANALYSIS_PATH=/ptmp/lscyboz/RivetCustomAnalyses/:$RIVET_ANALYSIS_PATH")
 
 
@@ -114,7 +126,7 @@ for i in range(nRuns):
 	spec='%03.0f' % (i,)
 	if not os.path.exists(dir+spec):
 	  os.system("mkdir -p "+dir+spec)
-	os.system("cp "+InputFolder+"emubB_homemade_NLO.in "+dir)
+	os.system("cp "+InputFolder+"emubB_matchbox_NLO.in "+dir)
 	if (i+1)%100==0: print "Processing run #"+str(i)
 
 	SubmitHerwigJob(nEvPerFile, i, "emubB_matchbox_NLO.run")
