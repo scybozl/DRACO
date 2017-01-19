6329b5c89073e16501b68b1afbd0e69098518985
Ludovic Michel Scyboz <lscyboz@draco02.mpcdf.mpg.de>
2017-01-19 11:04:26 +0100

    Helicity-level GoSam generation to overcome memory limit

--- GoSamGenerate.py
+++ GoSamGenerate.py
@@ -1,5 +1,6 @@
 import os
 sampledPars = "/ptmp/lscyboz/HERWIG/Herwig-scratch/Build/GoSam/source/"
+selecPath   = "/ptmp/lscyboz/processSelection.in"
 
 def SubDirPath (d):
     return filter(os.path.isdir, [os.path.join(d,f) for f in os.listdir(d)])
@@ -9,18 +10,25 @@ def createSubmit(directory):
     os.system("cd /ptmp/lscyboz/")
 
     template=open("submit_GoSam.sh", 'r')
-    fileName="Submit_"+directory+".sh"
+    fileName="Submit_"+name+".sh"
     file=open(fileName, 'w')
 
     for line in template:
 	if line.find("cd")!=-1:
-	  file.write("cd "+sampledPars+directory)
+	  file.write("cd "+directory)
 	else:
 	  file.write(line) 
 
 
 for subdir in SubDirPath(sampledPars):
-    if subdir.find('nmubarepbbar')!=-1:
-	createSubmit(subdir.split("source/")[1])
-	os.system("sbatch Submit_"+subdir.split("source/")[1]+".sh")
+    if subdir.find('nmubarep')!=-1:
+      for hel in SubDirPath(subdir):
+	if hel.find("helicity")!=-1:
+
+		name=hel.split("source/")[1].split("/")[0]+"_"+hel.split("source/")[1].split("/")[1]
+		selec=open(selecPath, 'r')
+		for line in selec:
+		  if name.find(line.split("\n")[0])!=-1:
+		    createSubmit(hel)
+		    os.system("sbatch Submit_"+name+".sh")
 #    os.system("./yoda2aida "+subdir+"/out.yoda "+subdir+"/out.aida")
