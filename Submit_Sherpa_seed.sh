021082b0c805a8b548d29184b9bbeb4e95fc3205
Ludovic Michel Scyboz <lscyboz@draco03.mpcdf.mpg.de>
2017-01-31 15:38:07 +0100

    New template for Sherpa event generation on MPI interface

---
+++ Submit_Sherpa_seed.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -l
+# Standard output and error:
+#SBATCH -o ./tjob.out.%j
+#SBATCH -e ./tjob.err.%j
+# Initial working directory:
+#SBATCH -D ./
+# Job Name:
+#SBATCH -J sherpa_generate
+# Queue (Partition):
+#SBATCH --partition=general
+# Number of nodes and MPI tasks per node:
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=16
+#
+#SBATCH --mail-type=none
+#SBATCH --mail-user=<userid>@rzg.mpg.de
+#
+# Wall clock limit:
+#SBATCH --time=24:00:00
+
+# Run the program:
+cd /ptmp/lscyboz/SHERPA-MC-2.2.0/emubB
+export PATH=/ptmp/lscyboz/SHERPA-MC-2.2.0/bin:$PATH
+export RIVET_ANALYSIS_PATH=/ptmp/lscyboz/RivetCustomAnalyses:$RIVET_ANALYSIS_PATH
+
+srun Sherpa -f Run_emubB.dat -e 10 -A 27.01.17.seed
